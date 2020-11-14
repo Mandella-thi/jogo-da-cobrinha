86348fc6f40770d80f9d94b4d3776b795dff4974
Mandella-thi <thiagogarc@gmail.com>
2020-11-13 22:43:05 -0300

    movimento da cobrinha

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0]={
     x:8 * box,
     y: 8 * box
 }
+let direction ="right";
 function criarBG(){
     context.fillStyle ="lightgreen"
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -17,5 +18,23 @@ function criarcobrinha(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
-criarBG()
-criarcobrinha();
+function iniciarjogo(){
+    criarBG();
+    criarcobrinha();
+
+    let snakeX= snake[0].x;
+    let snakeY= snake[0].y;
+    if(direction=="right") snakeX +=box;
+    if(direction== "left") snakeX -= box;
+    if(direction=="up") snakeY -=box
+    if(direction=="down") snakeY +=box;
+
+    snake.pop();
+
+    let newHead={
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newhead);
+}
+let jogo =setInterval(iniciarjogo, 100)
